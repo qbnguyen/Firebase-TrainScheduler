5af86c90457f7f1a846d665094662fd7e4390ef4
Quy Nguyen <nguyenqb24@gmail.com>
2018-09-13 22:14:59 -0400

    moment.js

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -17,12 +17,17 @@ $("#add-train-btn").on("click", function(event){
     var destination = $("#destination-input").val().trim();
     var startTrain = $("#start-input").val().trim();
     var frequency = $("#frequency-input").val().trim();
+    var startTrainUnix = moment(startTrain, "HH:mm").format("X");
 
     console.log(trainName)
     console.log(destination)
     console.log(startTrain)
     console.log(frequency)
+    console.log(startTrainUnix)
 
+    if (trainName && destination && startTrain && frequency){
+
+    
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#start-input").val("");
@@ -31,11 +36,15 @@ $("#add-train-btn").on("click", function(event){
     database.ref().push({
         trainName: trainName,
         destination: destination,
-        startTrain: startTrain,
-        frequency: frequency,
-        nextTrain: nextTrain,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        startTrainUnix: startTrainUnix,
+        frequency: frequency
+        
     });
+    }
+
+    else{
+        console.log("empty form");
+    }
 });
 
 database.ref().on("child_added", function(snapshot) {
@@ -43,26 +52,57 @@ database.ref().on("child_added", function(snapshot) {
 
     console.log(sv.trainName);
       console.log(sv.destination);
-      console.log(sv.startTrain);
+      console.log(sv.startTrainUnix);
       console.log(sv.frequency);
 
-    var table =$("tr");
+    var startTimeObj = moment.unix(sv.startTrainUnix);
+    console.log(startTimeObj);
+
+//startTime 12pm
+//frequency 10 mins
+//now 3:03 mil 15:03
+//3:03(60*3=180)+3=183mins
+// 183/frequency=183/10=18 remainder 3
+//frequency - remainder= 10-3 =7 til next train 
+
+    var now= moment();
+    console.log (now)
+    var diff= now.diff(startTimeObj, "minutes");
+    console.log(diff)
+    var remainder= diff % sv.frequency
+    console.log(remainder)
+    var minNextTrain= sv.frequency - remainder;
+    console.log(minNextTrain);
+    var nextArrivalObj= now.add(minNextTrain, "minutes");
+    console.log(nextArrivalObj);
+    var nextArrival= moment(nextArrivalObj).format("HH:mm");
+    console.log(nextArrival);
+
+
+
+
+
+
+
+
+
+    var table =$("<tr>");
 
     var tName = $("<td>").text(sv.trainName);
     var tDest = $("<td>").text(sv.destination);
-    var tStart = $("<td>").text(sv.startTrain);
     var tFrequency = $("<td>").text(sv.frequency);
-    var tNextTrain = $("<td>").text(sv.nextTrain);
-    
+    var tNextTrain = $("<td>").text(nextArrival);
+    var minAway = $("<td>").text(minNextTrain);
     
     table.append(tName);
     table.append(tDest);
-    table.append(tStart);
     table.append(tFrequency);
     table.append(tNextTrain);
+    table.append(minAway);
 
+    $("tbody").append(table);
 });
 
-var startTime = tStart;
-var timeFormat = moment("HH:mm");
-var remainingTime = tNextTrain;
+// var startTime = tStart;
+// var timeFormat = moment("HH:mm");
+// var remainingTime = tNextTrain;
